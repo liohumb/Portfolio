4a127aef2596a207c4bb7f437103ac8182ca0c82
Lionel Humbert <helloliohumb@gmail.com>
2021-07-17 00:00:19 +0200

    sauvegarde

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,7 +24,6 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-
 //TITRE HOME
 class TextScramble {
     constructor(el) {
@@ -32,79 +31,96 @@ class TextScramble {
       this.chars = '!<>-_\\/[]{}—=+*^?#________'
       this.update = this.update.bind(this)
     }
-    setText(newText) {
-      const oldText = this.el.innerText
-      const length = Math.max(oldText.length, newText.length)
-      const promise = new Promise((resolve) => this.resolve = resolve)
-      this.queue = []
-      for (let i = 0; i < length; i++) {
-        const from = oldText[i] || ''
-        const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 50)
-        const end = start + Math.floor(Math.random() * 50)
-        this.queue.push({ from, to, start, end })
+
+setText(newText) {
+  const oldText = this.el.innerText
+  const length = Math.max(oldText.length, newText.length)
+  const promise = new Promise((resolve) => this.resolve = resolve)
+
+  this.queue = []
+
+  for (let i = 0; i < length; i++) {
+    const from = oldText[i] || ''
+    const to = newText[i] || ''
+    const start = Math.floor(Math.random() * 60)
+    const end = start + Math.floor(Math.random() * 60)
+    this.queue.push({ from, to, start, end })
+  }
+
+  cancelAnimationFrame(this.frameRequest)
+
+  this.frame = 0
+  this.update()
+
+  return promise
+}
+
+update() {
+  let output = ''
+  let complete = 0
+
+  for (let i = 0, n = this.queue.length; i < n; i++) {
+
+    let { from, to, start, end, char } = this.queue[i]
+    
+    if (this.frame >= end) {
+      complete++
+      output += to
+    } else if (this.frame >= start) {
+      if (!char || Math.random() < 0.28) {
+        char = this.randomChar()
+        this.queue[i].char = char
       }
-      cancelAnimationFrame(this.frameRequest)
-      this.frame = 0
-      this.update()
-      return promise
-    }
-    update() {
-      let output = ''
-      let complete = 0
-      for (let i = 0, n = this.queue.length; i < n; i++) {
-        let { from, to, start, end, char } = this.queue[i]
-        if (this.frame >= end) {
-          complete++
-          output += to
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {
-            char = this.randomChar()
-            this.queue[i].char = char
-          }
-          output += `<span class="home_title-js">${char}</span>`
-        } else {
-          output += from
-        }
-      }
-      this.el.innerHTML = output
-      if (complete === this.queue.length) {
-        this.resolve()
-      } else {
-        this.frameRequest = requestAnimationFrame(this.update)
-        this.frame++
-      }
-    }
-    randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)]
+
+      output += `<span class="home_title-js">${char}</span>`
+
+    } else {
+      output += from
     }
   }
-  
-  const phrases = [
-    '_Hello,',
-    'my names is',
-    'Lio Humb',
-    '_and',
-    'I would like',
-    'to work',
-    'for you',
-    ':)'
-  ]
-  
-  const el = document.querySelector('.home_title')
-  const fx = new TextScramble(el)
-  
-  let counter = 0
-  const next = () => {
-    fx.setText(phrases[counter]).then(() => {
-      setTimeout(next, 800)
-    })
-    counter = (counter + 1) % phrases.length
+
+  this.el.innerHTML = output
+
+  if (complete === this.queue.length) {
+    this.resolve()
+  } else {
+    this.frameRequest = requestAnimationFrame(this.update)
+    this.frame++
   }
-  
-  next()
+}
 
+randomChar() {
+  return this.chars[Math.floor(Math.random() * this.chars.length)]
+}
 
+}
+
+const phrases = [
+  '_Hello,',
+  'my names is',
+  'Lio Humb',
+  '_and',
+  'I would like',
+  'to work',
+  'for you',
+  ':)'
+]
+
+const el = document.querySelector('.home_title')
+const fx = new TextScramble(el)
+
+let counter = 0
+
+const next = () => {
+  fx.setText(phrases[counter]).then(() => {
+    setTimeout(next, 800)
+  })
+
+  counter = (counter + 1) % phrases.length
+
+}
+
+next()
 
 // VARIABLES COMPETENCES
 const skillsContent = document.getElementsByClassName('skills_content'),
